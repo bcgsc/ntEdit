b30d2deef0375a597709461ff3912107b7fc7926
Rene Warren <rwarren@bcgsc.ca>
2019-03-26 19:44:03 -0700

    update to demo and readme
    Former-commit-id: fb05663c00ba58e56e88a1502f317aad9a75bbb0

--- demo/runme.sh
+++ demo/runme.sh
@@ -1,1 +1,9 @@
-../ntEdit.pl -f ecoliWithMismatches001Indels0001.fa.gz -r solidBF_k25.bf -k 25 -b ntEditEcolik25 
+#source /home/hmohamadi/.e7_env
+#RLW 03/2019
+#echo Running ntHits
+#==================
+#/usr/bin/time -v -o nthitk25.time nthits -c 2 -b 36 -k 25 --outbloom -p solidBF Sim_100_300_1.fq.gz Sim_100_300_2.fq.gz
+echo Running ntEdit...
+#===================
+/usr/bin/time -v -o nteditk25.time ../ntedit -f ecoliWithMismatches001Indels0001.fa.gz -k 25 -r solidBF_k25.bf -d 5 -i 4 -b nteditk25
+echo Output corrections in nteditk25_
