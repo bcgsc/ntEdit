82cdbdca313f9c3e3848f80c0b79ab71c8332036
Rene Warren <rwarren@bcgsc.ca>
2019-12-13 16:17:21 -0800

    moved data to bcgsc.ca
    Former-commit-id: db8da1e2d7b3a58d248bc66d6d99df17d5ec7db0

--- demo/runme.sh
+++ demo/runme.sh
@@ -2,6 +2,8 @@
 #RLW 03/2019
 #echo Running ntHits
 #==================
+#wget https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_1.fq.gz
+#wget https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_2.fq.gz
 #/usr/bin/time -v -o nthitk25.time nthits -c 2 -b 36 -k 25 --outbloom -p solidBF Sim_100_300_1.fq.gz Sim_100_300_2.fq.gz
 echo Running ntEdit...
 #===================
