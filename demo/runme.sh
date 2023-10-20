7723a54a19f30fa20aa16732f9b1ebbb541b16ff
parham-k <pkazemi3@gmail.com>
2023-10-20 12:34:20 -0700

    Update demo

--- demo/runme.sh
+++ demo/runme.sh
@@ -1,11 +1,16 @@
-#source /home/hmohamadi/.e7_env
-#RLW 03/2019
-#echo Running ntHits
-#==================
-#wget https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_1.fq.gz
-#wget https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_2.fq.gz
-#/usr/bin/time -v -o nthitk25.time nthits -c 2 -b 36 -k 25 --outbloom -p solidBF Sim_100_300_1.fq.gz Sim_100_300_2.fq.gz
-echo Running ntEdit...
-#===================
-/usr/bin/time -v -o nteditk25.time ../ntedit -f ecoliWithMismatches001Indels0001.fa.gz -r solidBF_k25.bf -d 5 -i 4 -b nteditk25
-echo Output corrections in nteditk25_
+#!/usr/bin/env bash
+
+set -e
+
+echo DOWNLOADING DATA
+wget -nc https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_1.fq.gz https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_2.fq.gz
+
+echo -e "\nRUNNING NTCARD"
+/usr/bin/time -v -o ntcard.time ntcard-old -k 25 -c 64 -p ntcard Sim_100_300_1.fq.gz Sim_100_300_2.fq.gz
+
+echo -e "\nRUNNING NTHITS"
+/usr/bin/time -v -o nthits.time nthits cbf -v -f ntcard_k25.hist -o nthits.cbf -k 25 --solid Sim_100_300_1.fq.gz Sim_100_300_2.fq.gz
+# /usr/bin/time -v -o nthits.time nthits bf -v -f ntcard_k25.hist -o nthits.bf -k 25 --solid Sim_100_300_1.fq.gz Sim_100_300_2.fq.gz
+
+echo -e "\nRUNNING NTEDIT"
+/usr/bin/time -v ../build/ntedit -f ecoliWithMismatches001Indels0001.fa.gz -r nthits.bf -b ntedit
