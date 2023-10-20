c0816fde1cb88a160ea2d00aa82ed2adc77bfb46
parham-k <pkazemi3@gmail.com>
2023-10-20 12:38:00 -0700

    Update demo script

--- demo/runme.sh
+++ demo/runme.sh
@@ -6,7 +6,7 @@ echo DOWNLOADING DATA
 wget -nc https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_1.fq.gz https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_2.fq.gz
 
 echo -e "\nRUNNING NTCARD"
-/usr/bin/time -v -o ntcard.time ntcard-old -k 25 -c 64 -p ntcard Sim_100_300_1.fq.gz Sim_100_300_2.fq.gz
+/usr/bin/time -v -o ntcard.time ntcard -k 25 -c 64 -p ntcard Sim_100_300_1.fq.gz Sim_100_300_2.fq.gz
 
 echo -e "\nRUNNING NTHITS"
 /usr/bin/time -v -o nthits.time nthits cbf -v -f ntcard_k25.hist -o nthits.cbf -k 25 --solid Sim_100_300_1.fq.gz Sim_100_300_2.fq.gz
