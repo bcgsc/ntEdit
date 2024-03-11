89eb57bac6bf3b39a27b6e747f9ed48217e04cea
lcoombe <lauren.e.coombe@gmail.com>
2024-03-11 10:28:57 -0700

    Update README.md, demo, run-ntedit

--- demo/runme.sh
+++ demo/runme.sh
@@ -5,4 +5,4 @@ set -e
 echo DOWNLOADING DATA
 wget -nc https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_1.fq.gz https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_2.fq.gz
 
-run-ntedit --bloomType cbf -k 31 --reads Sim_100_300 --draft ecoliWithMismatches001Indels0001.fa.gz
+run-ntedit polish -k 25 -d 5 -i 4 --reads Sim_100_300 --draft ecoliWithMismatches001Indels0001.fa.gz
