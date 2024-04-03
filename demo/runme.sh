2e1136e0fd06b22fc8619137e165a92a2362b01a
JW <34543031+jwcodee@users.noreply.github.com>
2024-04-03 13:11:38 -0700

    runme.sh: add file check

--- demo/runme.sh
+++ demo/runme.sh
@@ -6,3 +6,4 @@ echo DOWNLOADING DATA
 wget -nc https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_1.fq.gz https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_2.fq.gz
 
 run-ntedit polish -k 25 -d 5 -i 4 --reads Sim_100_300 --draft ecoliWithMismatches001Indels0001.fa.gz
+diff -q ntedit_k55_changes.tsv ecoli_ntedit_k25_changes.tsv
