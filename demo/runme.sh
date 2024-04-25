17488b9efbc3823bcee473519cdca1fcfca5258b
lcoombe <lauren.e.coombe@gmail.com>
2024-04-25 08:47:13 -0700

    Update demo files, bump all versions to 2.0.2

--- demo/runme.sh
+++ demo/runme.sh
@@ -6,4 +6,5 @@ echo DOWNLOADING DATA
 wget -nc https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_1.fq.gz https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_2.fq.gz
 
 run-ntedit polish -k 25 -d 5 -i 4 --reads Sim_100_300 --draft ecoliWithMismatches001Indels0001.fa.gz
+diff -q ntedit_k25_edited.fa ecoli_ntedit_k25_edited.fa 
 diff -q ntedit_k25_changes.tsv ecoli_ntedit_k25_changes.tsv
